bf249fe42a60e408d8a600d25ce0a83d2e217e2b
Rachel T <rachmaybe@gmail.com>
2019-08-29 03:20:28 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,5 +1,30 @@
 class Cat 
-  def name 
-    @name 
-  end 
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @asleep = false
+    @age = 0
+  end
+
+  def speak
+    "#{@name} meows."    
+  end
+  
+  def go_to_bed
+    @asleep = true
+  end
+  
+  def wake_up
+    @asleep = false   
+  end
+  
+  def asleep?
+    @asleep
+  end
+  
+  def get_older
+    @age += 1
+  end
+  
 end 
